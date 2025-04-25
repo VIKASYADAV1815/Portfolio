7b7b7b67ce68c59a1a57886c71055e6bb72e10f3
Your Name <your.email@example.com>
2025-04-25 22:19:54 +0530

    Updated portfolio

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-scroll'; // react-scroll ka Link use kar
 import './Home.css';
-import my from '../../assets/port.jpeg';
+import my from '../../assets/dp.jpg';
 import TypingEffect from 'react-typing-effect';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import gsap from 'gsap';
